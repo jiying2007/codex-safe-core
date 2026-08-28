ace1d2046163b8ea935d8310442a4428b32e59ab
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-08-28 19:02:53 +0800

    feat: export Codex runtime provider contract

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = Object.freeze({
   ...require('./safe-contract'),
+  ...require('./codex-runtime'),
   ...require('./process-runner'),
   ...require('./codex-cli'),
   ...require('./git-repository'),
